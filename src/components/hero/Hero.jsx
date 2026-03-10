0ad2d40d2bfdca8a49f12a88e717364a7275eda5
FaranRaja <faranraja011@gmail.com>
2026-03-10 17:29:34 +0500

    some changes

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -8,6 +8,19 @@ const Hero = () => {
 
       <div className="hero-main">
         <img src="https://tulos-sqq5.vercel.app/assets/hero1-DUrvbYh8.webp" alt="" srcset="" className='hero-img'/>
+
+
+        <div className="hero-content">
+          <div className="left-hero">
+            <h2>CARTLY SPRING COLLECTION</h2>
+            <span>Find out our best spring collection. Offering our best quality product in a Tolus
+              Spring Collection.</span>
+          </div>
+
+          <div className="hero-right">
+            <button className="shop">BUY NOW</button>
+          </div>
+        </div>
       </div>
      
       
